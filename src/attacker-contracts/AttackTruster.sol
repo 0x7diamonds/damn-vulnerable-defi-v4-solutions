d71ad8c6a6ee24f3acb46683a750ce04cb6d505d
itsnico222 <togiabao222@gmail.com>
2024-08-12 22:53:50 +0700

    attack

--- src/attacker-contracts/AttackTruster.sol
+++ src/attacker-contracts/AttackTruster.sol
@@ -4,3 +4,8 @@ pragma solidity ^0.8.0;
 
 import "../truster/TrusterLenderPool.sol"; 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract AttackTruster {
+    TrusterLenderPool public pool;
+    IERC20 public token;
+}
