f80008f40a85a712ea540b75bf750c09ef948f05
itsnico222 <togiabao222@gmail.com>
2024-08-16 03:12:15 +0700

    c

--- src/attacker-contracts/AttackSideEntrance.sol
+++ src/attacker-contracts/AttackSideEntrance.sol
@@ -1,3 +1,11 @@
 // SPDX-License-Identifier: MIT
  pragma solidity 0.8.25;
- 
+
+ import "../side-entrance/SideEntranceLenderPool.sol";
+
+ contract AttackSideEntrance {
+    SideEntranceLenderPool pool;
+    function attack() external {
+    
+    }
+ }
