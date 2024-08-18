10b6c737d095b11a20836629479c7976ffa64e49
itsnico222 <togiabao222@gmail.com>
2024-08-18 19:37:53 +0700

    a

--- src/attacker-contracts/AttackTruster.sol
+++ src/attacker-contracts/AttackTruster.sol
@@ -2,6 +2,7 @@
 pragma solidity 0.8.25;
 
 import {DamnValuableToken} from "../../src/DamnValuableToken.sol";
+import {TrusterLenderPool} from "../../src/truster/TrusterLenderPool.sol";
 contract AttackTruster {
 
 }
