b1db2a308cfe1a6504fd761ca0e786624442bc06
itsnico222 <togiabao222@gmail.com>
2024-08-18 19:39:06 +0700

    xc

--- src/attacker-contracts/AttackTruster.sol
+++ src/attacker-contracts/AttackTruster.sol
@@ -5,4 +5,13 @@ import {DamnValuableToken} from "../../src/DamnValuableToken.sol";
 import {TrusterLenderPool} from "../../src/truster/TrusterLenderPool.sol";
 contract AttackTruster {
 
+    DamnValuableToken public token;
+    TrusterLenderPool public pool;
+    address public recovery;
+
+    constructor(address _token, address _pool, address _recovery) {
+        token = DamnValuableToken(_token);
+        pool = TrusterLenderPool(_pool);
+        recovery = _recovery;
+    }
 }
