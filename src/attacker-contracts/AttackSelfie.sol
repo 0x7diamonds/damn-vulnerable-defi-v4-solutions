b5fde36aaf3145c7f9832cc8607c953bcaf33b1f
itsnico222 <togiabao222@gmail.com>
2024-08-29 00:02:07 +0700

    commit

--- src/attacker-contracts/AttackSelfie.sol
+++ src/attacker-contracts/AttackSelfie.sol
@@ -2,8 +2,8 @@
 
 pragma solidity =0.8.25;
 
-import "../selfie/SelfiePool.sol";
-import "../selfie/SimpleGovernance.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/interfaces/IERC3156FlashBorrower.sol";
 
 contract AttackSelfie {
     
