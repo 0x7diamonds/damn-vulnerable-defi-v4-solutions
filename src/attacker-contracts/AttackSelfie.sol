0e03ef1a5522c8edf1bd9393b86c101ad42a2a5c
itsnico222 <togiabao222@gmail.com>
2024-08-29 00:04:25 +0700

    flashloan

--- src/attacker-contracts/AttackSelfie.sol
+++ src/attacker-contracts/AttackSelfie.sol
@@ -5,6 +5,15 @@ pragma solidity =0.8.25;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/interfaces/IERC3156FlashBorrower.sol";
 
+interface IPool {
+    function flashLoan(
+        IERC3156FlashBorrower _receiver,
+        address _token,
+        uint256 _amount,
+        bytes calldata data
+    ) external returns (bool);
+}
+
 contract AttackSelfie {
     
 }
