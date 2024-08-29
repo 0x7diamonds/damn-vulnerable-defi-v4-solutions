276eaa59ea5c8fbd540eb87b344879279bb83bd3
itsnico222 <togiabao222@gmail.com>
2024-08-29 12:01:04 +0700

    a

--- src/attacker-contracts/AttackSelfie.sol
+++ src/attacker-contracts/AttackSelfie.sol
@@ -7,10 +7,11 @@ import {SimpleGovernance} from "../../src/selfie/SimpleGovernance.sol";
 import {SelfiePool} from "../../src/selfie/SelfiePool.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {IERC3156FlashBorrower} from "@openzeppelin/contracts/interfaces/IERC3156FlashBorrower.sol";
-import {DamnValuableToken} from "../../src/DamnValuableToken.sol";
+import {DamnValuableVotes} from "../../src/DamnValuableVotes.sol";
 
 contract AttackSelfie is IERC3156FlashBorrower {
 
     SelfiePool pool;
     SimpleGovernance governance;
+    DamnValuableToken 
 }
