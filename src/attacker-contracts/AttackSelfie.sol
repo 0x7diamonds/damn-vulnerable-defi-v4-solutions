1d89c39994cac3b5c73ed74284221b1c982f79d3
itsnico222 <togiabao222@gmail.com>
2024-08-29 11:58:27 +0700

    a

--- src/attacker-contracts/AttackSelfie.sol
+++ src/attacker-contracts/AttackSelfie.sol
@@ -4,3 +4,4 @@ pragma solidity =0.8.25;
 
 import {Test, console} from "forge-std/Test.sol";
 import {SimpleGovernance} from "../../src/selfie/SimpleGovernance.sol";
+import {SelfiePool} from "../../src/selfie/SelfiePool.sol";
