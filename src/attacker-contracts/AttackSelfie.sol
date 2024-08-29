6e4e1afb67af16029882bfe8c37f94bb595e39d6
itsnico222 <togiabao222@gmail.com>
2024-08-29 11:58:52 +0700

    a

--- src/attacker-contracts/AttackSelfie.sol
+++ src/attacker-contracts/AttackSelfie.sol
@@ -5,3 +5,4 @@ pragma solidity =0.8.25;
 import {Test, console} from "forge-std/Test.sol";
 import {SimpleGovernance} from "../../src/selfie/SimpleGovernance.sol";
 import {SelfiePool} from "../../src/selfie/SelfiePool.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
