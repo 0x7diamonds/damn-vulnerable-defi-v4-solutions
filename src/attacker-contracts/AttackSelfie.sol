f050eea353b8964935be99ad51082aecc60e7101
itsnico222 <togiabao222@gmail.com>
2024-08-30 22:47:17 +0700

    add

--- src/attacker-contracts/AttackSelfie.sol
+++ src/attacker-contracts/AttackSelfie.sol
@@ -59,7 +59,7 @@ contract AttackSelfie is IERC3156FlashBorrower {
             amountRequired,
             data
         );
-    }
+    } // add ß
 
     function attackCloseup() external {
         bytes memory resultData = governance.executeAction(actionId);
