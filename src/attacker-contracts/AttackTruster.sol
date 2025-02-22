b0b90b46263c51fd7fa7c88825364923ab4f927b
itsnico222 <togiabao222@gmail.com>
2025-02-22 15:02:12 +0700

    a

--- src/attacker-contracts/AttackTruster.sol
+++ src/attacker-contracts/AttackTruster.sol
@@ -4,3 +4,19 @@ pragma solidity ^0.8.23;
 
 import {DamnValuableToken} from "../../src/DamnValuableToken.sol";
 import {TrusterLenderPool} from "../../src/truster/TrusterLenderPool.sol";
+
+contract AttackTruster {
+    DamnValuableToken token;
+    TrusterLenderPool pool;
+    address public recovery;
+
+    constructor(DamnValuableToken token_, TrusterLenderPool pool_, address recovery_) {
+        token = token_;
+        pool = pool_;
+        recovery = recovery_;
+    }
+
+    function attack() external returns(bool) {
+        pool.flashLoan()
+    }
+}
