059b5ad03492b99f129e18d41c6a49675d9d87df
itsnico222 <togiabao222@gmail.com>
2025-03-05 16:17:08 +0700

    a

---
+++ src/attacker-contracts/AttackPuppet.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity =0.8.25;
+
+import {Test} from "forge-test/Test.sol";
+import {PuppetPool} from "../contracts/puppet/PuppetPool.sol";
+import {DamnValuableToken} from "../../src/DamnValuableToken.sol";
+import {IUniswapV1Factory} from "../contracts/puppet/IUniswapV1Factory.sol";
+import {IUinswapV1Exchange} from "../contracts/puppet/IUniswapV1Exchange.sol";
+
+contract AttackPuppet {
+
+}
